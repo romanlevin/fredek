2f5942354d22ce06921903357cfba106fe92066a
Roman Levin <romanlevin@gmail.com>
2014-01-03 16:27:29 -0500

    NewRelic and Google Analytics

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
+require('newrelic');
 
 // Module dependencies.
 var express = require('express'),
     path = require('path'),
     fs = require('fs');
-
 var app = express();
 
 // Connect to database
